be3e7892a0032b443bec1c08c5847cb49d946ce9
rizkysyahri <ramadhancai@gmail.com>
2025-06-04 20:18:37 +0700

    update vercel.json

--- backends/dist/index.js
+++ backends/dist/index.js
@@ -15,6 +15,7 @@ const corsConfig = {
     origin: [
         "http://localhost:3005",
         "https://meducalc.rftdigitalsolution.com",
+        "https://meducalc-api.rftdigitalsolution.com",
         "http://localhost:3000",
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
@@ -25,50 +26,6 @@ const corsConfig = {
 app.use((0, cors_1.default)(corsConfig));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
+app.set("trust proxy", true);
 app.use("/api/", schema1_1.default);
-// app.get("/api/mahasiswa", async (req: Request, res: Response) => {
-//   try {
-//     // Ambil data dan cast tgl_lahir jadi string pakai MySQL function
-//     const result = await prismaMysql.$queryRawUnsafe<any[]>(`
-//       SELECT
-//         id,
-//         angkatan,
-//         nim,
-//         nama_mahasiswa,
-//         tempat_lahir,
-//         CAST(tgl_lahir AS CHAR) AS tgl_lahir,
-//         alamat,
-//         hp,
-//         email,
-//         waktu_dibuat,
-//         waktu_dirubah,
-//         dibuat_oleh,
-//         dirubah_oleh,
-//         riwayat_perubahan
-//       FROM mda_master_mahasiswa
-//     `);
-//     const mahasiswa = result.map((item) => {
-//       let tgl_lahir_iso: string | null = null;
-//       if (item.tgl_lahir && item.tgl_lahir !== "0000-00-00") {
-//         const date = new Date(item.tgl_lahir);
-//         if (!isNaN(date.getTime())) {
-//           tgl_lahir_iso = date.toISOString();
-//         }
-//       }
-//       return {
-//         ...item,
-//         tgl_lahir: tgl_lahir_iso,
-//       };
-//     });
-//     res.status(200).json({
-//       success: true,
-//       data: mahasiswa,
-//     });
-//   } catch (error) {
-//     const err = error as Error;
-//     res.status(500).json({ message: err.message, status: false });
-//   }
-// });
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+exports.default = app;
